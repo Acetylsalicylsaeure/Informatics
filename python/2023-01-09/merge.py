4a67b04dd3779eb8ff155d238fdc0deb3db4e941
Acetylsalicylsaeure <110168604+Acetylsalicylsaeure@users.noreply.github.com>
2023-01-09 13:27:07 +0100

    2023-01-09

--- python/2023-01-09/merge.py
+++ python/2023-01-09/merge.py
@@ -1,1 +1,10 @@
 # todo: write function that merges 2 .txt files into newfile
+
+
+def mergefile(file1, file2, newfile):
+    with open(file1) as f1, open(file2) as f2:
+        output = list(f1) + list(f2)
+        print(output)
+
+
+mergefile("Files/hours.txt", "Files/person.txt", "new")
